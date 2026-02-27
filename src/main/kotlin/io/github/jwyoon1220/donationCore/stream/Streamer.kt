7206cb083261727fc970f39fe5b5390daa9cdf46
Junwon Yoon <jwyoon2010@gmail.com>
2026-02-27 16:08:26 +0900

    버그잡음

--- src/main/kotlin/io/github/jwyoon1220/donationCore/stream/Streamer.kt
+++ src/main/kotlin/io/github/jwyoon1220/donationCore/stream/Streamer.kt
@@ -26,6 +26,7 @@ import zzik2.soop4j.chat.event.NotificationEvent
 import zzik2.soop4j.model.channel.StationInfo
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.TimeUnit
+import java.util.logging.Logger
 
 class Streamer(
     val player: OfflinePlayer,
@@ -33,11 +34,14 @@ class Streamer(
     private val platform: Platform
 ) {
 
+    private val logger = Logger.getLogger("Streamer(id: $id)")
+
     private lateinit var chzzkChat: ChzzkChat
     private lateinit var soopChat: SoopChat
 
     private val listeners = ArrayList<StreamListener>()
 
+    private var isFirstConnect = true
 
     companion object {
         inline fun runMain(crossinline block: () -> Unit) {
@@ -62,7 +66,7 @@ class Streamer(
     }
 
     private fun connectToChzzk() {
-        chzzkChat = ChzzkChatBuilder(DonationCore.CHZZK_CLIENT, id).build()
+        chzzkChat = ChzzkChatBuilder(DonationCore.CHZZK_CLIENT, id).withAutoReconnect(true).build()
 
         chzzkChat.on(NormalDonationEvent::class.java) { event ->
 
@@ -91,12 +95,15 @@ class Streamer(
 
         try {
             chzzkChat.connectAsync()
+            logger.info("${player.name}: Connecting to chzzk... (id: $id)")
         } catch (e: Exception) {
+            logger.info("${player.name}: Exception on Connection ${e.javaClass.name}: ${e.message}")
             e.printStackTrace()
             player.player?.sendTranslatedMessage("&c치지직 연결중 오류가 발생했습니다. (${e.javaClass.name}: ${e.message})")
             player.player?.sendTranslatedMessage("&c StackTrace: ${e.stackTraceToString()}")
             return
         }
+        logger.info("${player.name}: Connected to chzzk.")
 
         player.player?.sendTranslatedMessage("&a치지직에 연결되었습니다. (ChID: $id, name: ${DonationCore.CHZZK_CLIENT.fetchChannel(id).channelName})")
     }
@@ -111,13 +118,10 @@ class Streamer(
         val name = stationInfo.station.stationName
 
         soopChat.addListener(object : SoopChatListener {
-            override fun onConnect(event: ConnectEvent) {
-                player()?.sendTranslatedMessage("SOOP에 연결되었습니다. (Streamer ID: $id, name: $name")
-            }
             override fun onDonation(event: DonationEvent) {
                 val donation = Donation(
                     type = "SOOP_DONATION",
-                    nickname = event.fromUsername ?: "익명",
+                    nickname = event.fromUsername ?: event.from ?: "Unknown",
                     message = "", // TODO: SOOP은 별풍선, 애드벌룬 이벤트에 메시지가 있지 않고 TTS가 이 이벤트 후 바로 오는 채팅 이벤트를 출력.
                     payAmount = event.amount,
                     id = this@Streamer.id
@@ -127,11 +131,16 @@ class Streamer(
         })
         try {
             soopChat.connectAsync()
+            logger.info("${player.name}: Connecting to soop...")
         } catch (e: Exception) {
+            logger.info("${player.name}: Exception on Connection ${e.javaClass.name}: ${e.message}")
             e.printStackTrace()
             player()?.sendTranslatedMessage("&cSOOP 연결중 오류가 발생했습니다. (${e.javaClass.name}: ${e.message})")
             player()?.sendTranslatedMessage("&cStackTrace: ${e.stackTraceToString()}")
+            return
         }
+        logger.info("${player.name}: Connected to soop.")
+        player()?.sendTranslatedMessage("SOOP에 연결되었습니다. (BJID: $id, name: $name)")
     }
 
     /** 후원 이벤트 전달 */
